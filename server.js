6631d36d47bb0c3aa4df1606d8c9ca9ebe64d828
Argir Argirov <dj.giro@gmail.com>
2018-03-16 15:38:57 -0400

    partials and helpers injection

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const hbs = require('hbs')
 
 let app = express()
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hi express</h1>')
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeText: 'hey guys, node is super cool'
 
   })
@@ -19,7 +26,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   })
 })
 
